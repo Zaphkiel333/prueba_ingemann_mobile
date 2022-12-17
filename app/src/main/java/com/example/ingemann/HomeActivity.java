bf1bd562d61669b7337cd97118ef2be697b93c8a
Rafael Hernandez <rhhs333@gmail.com>
2022-12-17 05:51:42 -0600

    HomeActivity creado, transicion de fragments creado

---
+++ app/src/main/java/com/example/ingemann/HomeActivity.java
@@ -0,0 +1,55 @@
+package com.example.ingemann;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.ingemann.Fragments.CreateInvoiceFragment;
+import com.example.ingemann.Fragments.ViewInvoicesFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    CreateInvoiceFragment createInvoiceFragment = new CreateInvoiceFragment();
+    ViewInvoicesFragment viewInvoicesFragment = new ViewInvoicesFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        bindUI();
+
+    }
+
+    private void bindUI(){
+        BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
+        loadFragment(createInvoiceFragment);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.createInvoiceFragment:
+                    loadFragment(createInvoiceFragment);
+                    return true;
+                case R.id.viewInvoicesFragment:
+                    loadFragment(viewInvoicesFragment);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void loadFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.commit();
+    }
+}
--- app/src/main/java/com/example/ingemann/MainActivity.java
+++ app/src/main/java/com/example/ingemann/MainActivity.java
@@ -2,15 +2,39 @@ package com.example.ingemann;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txtUsername, txtPassword;
+    private String username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportActionBar().hide();
+        bindUI();
+    }
+
+    private void bindUI() {
+        txtUsername = findViewById(R.id.txt_username);
+        txtPassword = findViewById(R.id.txt_password);
+    }
+
+    public void signIn(View view) {
+        username = String.valueOf(txtUsername.getText());
+        password = String.valueOf(txtPassword.getText());
+
+        Intent intent = new Intent(this,HomeActivity.class);
+        startActivity(intent);
     }
 }
